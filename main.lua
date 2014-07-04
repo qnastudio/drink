118c51a2e5aa3069a5d20dc4c62dbbb82fcafd55
Calvin-Lin <calvinlinos@gmail.com>
2014-07-04 14:09:12 +0800

    add main.lua

---
+++ main.lua
@@ -0,0 +1,36 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+display.setStatusBar( display.HiddenStatusBar )
+
+local storyboard = require( "storyboard" )
+storyboard.gotoScene( "start3" )
+
+actW = display.actualContentWidth
+actH = display.actualContentHeight
+conCX = display.contentCenterX
+conCY = display.contentCenterY
+
+teaN = 8
+iceN = 8
+sugarN = 8
+teaCatch = 0
+iceCatch = 0
+sugarCatch = 0  
+
+
+--need improve
+cupW,cupH = 200,320
+cupX,cupY = display.viewableContentWidth/2,display.viewableContentWidth/1.8+10
+toScene = false
+temp = 1
+
+
+storyboard.parameters = {
+	sceneChangTime = 300
+}
+-------------------------------
+
+display.setDefault( "background", 1)
